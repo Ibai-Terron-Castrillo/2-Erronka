fe003619978e33adc8243607293a1daee9a5c1fa
Aitor Auzmendi <aitorauzmen@gmail.com>
2025-01-27 10:26:27 +0100

    Klaseak aldatuta

--- Erronka/src/erronka/bezeroa.java
+++ Erronka/src/erronka/bezeroa.java
@@ -1,5 +1,21 @@
 package erronka;
 
-public class bezeroa {
+import java.util.ArrayList;
+import java.util.List;
 
+public class bezeroa extends pertsona {
+    private int idBezeroa;
+    private List<eskaera> eskaerak;
+
+    public  bezeroa(String izena, String abizenak, String erabiltzailea, String pasahitza, String email, String telefonoa, String nan, int idBezeroa) {
+        super(izena, abizenak, erabiltzailea, pasahitza, email, telefonoa, nan);
+        this.idBezeroa = idBezeroa;
+        this.eskaerak = new ArrayList<>();
+    }
+
+    public void addEskaera(eskaera eskaera) {
+        eskaerak.add(eskaera);
+    }
+
+    // Getters y setters
 }
--- Erronka/src/erronka/eskaera.java
+++ Erronka/src/erronka/eskaera.java
@@ -1,5 +1,21 @@
 package erronka;
 
-public class eskaera {
+import java.util.Date;
 
+public class eskaera {
+    private int idEskaera;
+    private Date data;
+    private String helbidea;
+    private String eskaeraEgoera;
+    private int idGarraiolaria;
+
+    public eskaera(int idEskaera, Date data, String helbidea, String eskaeraEgoera, int idGarraiolaria) {
+        this.idEskaera = idEskaera;
+        this.data = data;
+        this.helbidea = helbidea;
+        this.eskaeraEgoera = eskaeraEgoera;
+        this.idGarraiolaria = idGarraiolaria;
+    }
+
+    // Getters y setters
 }
